3340ae7dfe160ff99861e981cfd4d85c86eabd4a
Niklas Arndt <niklasarndt28@gmail.com>
2020-07-26 19:26:41 +0200

    Fixing #2
    Signed-off-by: Niklas Arndt <niklasarndt28@gmail.com>

---
+++ src/main/java/com/niklasarndt/discordbutler/modules/fun/command/GimmeCommand.java
@@ -0,0 +1,21 @@
+package com.niklasarndt.discordbutler.modules.fun.command;
+
+import com.niklasarndt.discordbutler.modules.ButlerCommand;
+import com.niklasarndt.discordbutler.modules.ButlerContext;
+
+/**
+ * Created by Niklas on 2020/07/26
+ */
+public class GimmeCommand extends ButlerCommand {
+
+    public GimmeCommand() {
+        super("gimme", 1, 10,
+                "Let the butler serve you something delicious (e.g. gimme sausage).", "give");
+    }
+
+    @Override
+    public void execute(ButlerContext context) {
+        context.resultBuilder().setOutput(String.format("Of course, please enjoy your `%s`.",
+                String.join(" ", context.args()).trim()));
+    }
+}
